b71323d272b2f8408eb2c8c99bbb19dfd687c5ff
habibadnaan <hello.habibadnaan@gmail.com>
2026-08-28 18:27:06 +0600

    Some functions are modified.

--- script.js
+++ script.js
@@ -32,9 +32,40 @@ function toggleStyle(id){
     thrivingfilterBtn.classList.add('bg-gray-300','text-black')  
     strugglingFilterBtn.classList.add('bg-gray-300','text-black')  
 
+ 
     const selected = document.getElementById(id);
 
-selected.classList.remove('bg-gray-300','text-black');
-selected.classList.add('bg-black', 'text-white');
+    selected.classList.remove('bg-gray-300','text-black');
+    selected.classList.add('bg-black', 'text-white');
 }
+
+mainCointainer.addEventListener('click', function(event){
+ 
+    const parentNode = event.target.parentNode.parentNode;
+    const plantName = parentNode.querySelector('.plant-name').innerText;
+    const light = parentNode.querySelector('.light').innerText;
+    const water = parentNode.querySelector('.water').innerText;
+    const status = parentNode.querySelector('.status').innerText;
+    const notes = parentNode.querySelector('.notes').innerText;
+    console.log(plantName, light,water,status,notes);
+
+    const cardInfo = {
+        plantName, 
+        light,
+        water,
+        status,
+        notes
+    }
+
+   
+
+    const plantExist = thrivingList.find(items=> items.plantName == cardInfo.plantName);
+if(plantExist){
+    thrivingList.push(cardInfo); 
+}
+
+console.log(thrivingList);
+
+});
+
 
